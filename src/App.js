86a3b15764f6eb60c41a4dbb4b7fb15758901120
Mark Brian <markbkiunga2017@gmail.com>
2024-08-13 19:14:06 +0300

    Adds header and updates website title and logo and solves error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Discover from './components/Discover/Discover';
 
 function App() {
   return (
     <div className="App">
+      <Discover />
     </div>
   );
 }
---
+++ src/components/Discover/Discover.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Discover() {
+  return (
+    <div>
+      <div id="content-container">
+        <img src="/SAFIRI LOGO.png" alt="safiri-logo" title="safiri-logo" />
+        <h1>Discover</h1>
+      </div>
+
+      <div id="saved-content-container"></div>
+    </div>
+  );
+}
+
+export default Discover;
