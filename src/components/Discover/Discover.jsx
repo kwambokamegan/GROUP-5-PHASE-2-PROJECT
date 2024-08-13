1d02ab8ecc511e4c1fafbc92e12b15935d507bd7
Mark Brian <markbkiunga2017@gmail.com>
2024-08-13 20:45:20 +0300

    Implements addition to Saved Sidepanel

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -1,28 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import './Discover.css';
 function Discover() {
+  //Places State
   const [places, setPlaces] = useState([]);
-
+  //Fetch Places
   useEffect(() => {
     fetch('https://safiri-backend.vercel.app/places')
       .then((response) => response.json())
       .then((placesData) => setPlaces(placesData));
   }, []);
 
-  console.log(places);
-
+  //   console.log(places);
+  //Display places by iterating through places
   const displayPlaces = places.map((place) => {
     return (
       <div className="place-card" key={place.id}>
-        <video autoPlay muted width="500px" height="500px">
+        <video
+          autoPlay
+          muted
+          loop
+          className="place-video"
+          id={place.id}
+          onClick={handlePlaceClick}
+        >
           <source src={place.image} type="video/mp4"></source>
         </video>
         <h1>{place.title}</h1>
-        <p>{place.description}</p>
-        <p>{place.activities}</p>
+        <h4>{place.description}</h4>
+        <h5>{place.activities}</h5>
+        {/* <h5>{place.transport_options}</h5> */}
       </div>
     );
   });
+
+  //Save a place
+  const [savedPlaces, setSavedPlaces] = useState([]);
+  function handlePlaceClick(event) {
+    console.log(savedPlaces);
+    const placeId = event.target.id;
+    console.log(placeId);
+    fetch(`https://safiri-backend.vercel.app/places/${placeId}`)
+      .then((response) => response.json())
+      .then((place) => {
+        if (!savedPlaces.some((savedPlace) => savedPlace.id === place.id)) {
+          setSavedPlaces((prevState) => [...prevState, place]);
+        }
+      });
+  }
+
+  //Add a saved place to the Saved places section
+  const displaySavedPlaces = savedPlaces.map((savedPlace) => {
+    return (
+      <div className="saved-place-card" key={savedPlace.id}>
+        <h1>{savedPlace.title}</h1>
+        <h4>{savedPlace.description}</h4>
+        {/* // <h5>{savedPlace.activities}</h5> */}
+      </div>
+    );
+  });
+
   return (
     <div id="discover-container">
       <div id="content-container">
@@ -31,7 +67,7 @@ function Discover() {
         <div id="places-container">{displayPlaces}</div>
       </div>
 
-      <div id="saved-content-container"></div>
+      <div id="saved-content-container">{displaySavedPlaces}</div>
     </div>
   );
 }
