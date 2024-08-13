5eeeab894820057f8a98eb1d80407576bd688713
Mark Brian <markbkiunga2017@gmail.com>
2024-08-13 19:42:28 +0300

    Adds places data to DOM and starts styling

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import './Discover.css';
 function Discover() {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    fetch('https://safiri-backend.vercel.app/places')
+      .then((response) => response.json())
+      .then((placesData) => setPlaces(placesData));
+  }, []);
+
+  console.log(places);
+
+  const displayPlaces = places.map((place) => {
+    return (
+      <div className="place-card" key={place.id}>
+        <video autoPlay muted width="500px" height="500px">
+          <source src={place.image} type="video/mp4"></source>
+        </video>
+        <h1>{place.title}</h1>
+        <p>{place.description}</p>
+        <p>{place.activities}</p>
+      </div>
+    );
+  });
   return (
-    <div>
+    <div id="discover-container">
       <div id="content-container">
         <img src="/SAFIRI LOGO.png" alt="safiri-logo" title="safiri-logo" />
         <h1>Discover</h1>
+        <div id="places-container">{displayPlaces}</div>
       </div>
 
       <div id="saved-content-container"></div>
