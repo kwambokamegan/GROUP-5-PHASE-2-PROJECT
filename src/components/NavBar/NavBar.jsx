5a3563583031cd398f960391b71860364a914773
KipkorirA <aron.kipkorir@student.moringaschool.com>
2024-08-14 11:11:48 +0300

    Implement responsive navbar with scroll effects and enhanced link styles
    - Added a fixed navbar that hides links on scroll down and reveals them on scroll up.
    - Applied background color and hover effects to navbar links, including color change and removal of underline on hover.
    - Integrated useEffect hook to manage scroll behavior in the NavBar component.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import {useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'; 
 import logo from '../pictures/logo.png';
 
 const NavBar = () => {
+
+  useEffect(() => {
+    let lastScrollTop = 0;
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function() {
+      const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (currentScroll > lastScrollTop) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+      lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    });
+  }, []);
+
   return (
     <nav className="navbar">
       <img src={logo} alt="Site Logo" className="navbar-logo" /> 
